6c37ee70ca63653a2bb0bbafc5bda0a418a4fb6c
kaviyarasan <kaviyarasan.t@twilightitsolutions.com>
2023-02-28 19:00:40 +0530

    fix: icons issues

--- src/app/shared/interceptor/httpInterceptor.ts
+++ src/app/shared/interceptor/httpInterceptor.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtb2JpbGVfbnVtYmVyIjoiODUwODg4MzIzNyIsImV4cCI6MTY4MTU1NDM5N30.BKu6GaRXAxr8-tsMTL57IC7dfGD3a1GkjQrf9dd8x14';
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtb2JpbGVfbnVtYmVyIjoiODUwODg4MzIzNyIsImV4cCI6MTY4MjQ0ODQ2M30.LWlE_NtbXZqD_KetfypCXnmMmRfnLPZaylBwwIpe6hI';
 @Injectable()
 export class Interceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
